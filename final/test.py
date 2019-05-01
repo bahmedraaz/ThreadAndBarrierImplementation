b84ca405fba396ce5eadf58b9cd945b49f73e51b
Bulbul <raaz.04eee@gmail.com>
2019-05-01 00:02:27 -0400

    moved test.py into final folder

--- final/test.py
+++ final/test.py
@@ -1,5 +1,5 @@
-#This script takes two arguments. For example, if you type "python test.py 10 2", it will first create an input file named inputFile.txt 
-#having 10 inputs and run the program for 2 iteration
+# If you run the test.py, it will ask you the number of inputs and number of iterations
+# This script will first create an input file named inputFile.txt having expected number of inputs and run the program for expected number of iteration iterations
 
 from subprocess import call
 import os
@@ -7,17 +7,22 @@ import random
 import time
 import sys
 
+numInput = input("Enter the number of Inputs\n")
+numInput = int(numInput)
+numIteration = input("Please enter the number of iterations\n")
+numIteration = int(numIteration)
+
 command = "./max"
 rmCommand = "rm "+"inputFile.txt"
 
 #print(len(sys.argv))
 
-if len(sys.argv)<3:
-	print("Please enter the number of iteration as a command line argument\n")
-	exit()
+#if len(sys.argv)<3:
+#	print("Please enter the number of iteration as a command line argument\n")
+#	exit()
 
-numIteration = int(sys.argv[2])
-numInput = int(sys.argv[1])
+#numIteration = int(sys.argv[2])
+#numInput = int(sys.argv[1])
 
 for _ in range(numIteration):
 	
